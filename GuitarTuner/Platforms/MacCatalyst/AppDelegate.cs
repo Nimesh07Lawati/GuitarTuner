e42c9910d211d3d06e9cc261b45ecd8907fc1229
Nimesh07Lawati <sangealawati@gmail.com>
2025-03-26 13:35:53 +0545

    Add .NET MAUI project files

---
+++ GuitarTuner/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace GuitarTuner;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
